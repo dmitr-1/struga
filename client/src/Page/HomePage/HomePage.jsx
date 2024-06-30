e8d3c873a775e0499573aac1d6650861d7e6c6b0
dmitr-1 <Vzhigd@yandex.ru>
2024-06-30 13:12:20 +0700

    делаю о мастерской

--- client/src/Page/HomePage/HomePage.jsx
+++ client/src/Page/HomePage/HomePage.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import VideoPlay from '../../Components/VideoPlay';
 import Navbar from '../Navbar/Navbar';
 import CatalogImg from '../../Components/CatalogImg';
+import img1About from '../images/imageAbout1.png';
+import img2About from '../images/imageAbout2.png';
+import Clients from '../../Components/Clients';
 
 export default function HomePage() {
   return (
@@ -56,6 +59,45 @@ export default function HomePage() {
           <button type='submit' className='btnCatalog'>
             СМОТРЕТЬ ВЕСЬ КАТАЛОГ
           </button>
+        </div>
+      </div>
+      <div className='aboutMaster'>
+        <div style={{ width: '80vw', margin: '0 auto' }}>
+          <div className='aboutMasterJScontent'>
+            <div>
+              <div className='aboutTitle'>О нашей мастерской</div>
+              <div className='discriptonAbout'>
+                Struga — творческая столярная мастерская, ручное производство
+                деревянной посуды и кухонной утвари, предметов декора, элементов
+                интерьера и мебели, подарков, сувениров. Для дома и бизнеса.
+                Наши изделия изготовлены из отборной, хорошо высушенной
+                древесины твердых пород, тщательно отшлифованы и пропитаны
+                натуральными маслами. Мы любим дерево, для нас важны детали,
+                ценим профессиональный подход, не экономим на материалах и
+                инструментах, поэтому гарантируем долговечность произведений
+                мастерской.
+              </div>
+              <div className='discriptonAbout'>
+                История мастерской Struga началась в 2010 году с хобби —
+                изготовление оригинальных фоторамок из состаренных сосновых
+                дощечек на подарки для друзей. Начиная с 2015 столярная
+                мастерская уже становится основным занятием, приносящим
+                заработок. В процессе работы и творчества осваивались новые
+                технологии, приемы и инструменты.   Пробовали изготавливать
+                разделочные доски, тарелки, шкатулки, светильники. Со временем
+                мастерская Struga обзавелась своим помещением и серьезным
+                арсеналом станков и инструментов, преобразовалась во вполне
+                удачный коммерческий проект с интересными работами.
+              </div>
+            </div>
+            <div className='imgAbout'>
+              <img src={img1About} className='tgtAbout1' />
+              <img src={img2About} className='tgtAbout2' />
+            </div>
+          </div>
+          <div className='aboutClient'>Наши клиенты</div>
+          <Clients />
+          <div className='aboutReciew'>Отзывы</div>
         </div>
       </div>
     </div>
