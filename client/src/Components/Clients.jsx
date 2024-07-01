f57343dc95839fc0055b3da5fe550bcad5956e90
dmitr-1 <Vzhigd@yandex.ru>
2024-07-01 12:52:09 +0700

    ready done

--- client/src/Components/Clients.jsx
+++ client/src/Components/Clients.jsx
@@ -1,29 +1,77 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import client1 from '../Page/images/client1.svg';
 import client2 from '../Page/images/client2.svg';
 import client3 from '../Page/images/client3.svg';
 import client4 from '../Page/images/client4.svg';
 import client5 from '../Page/images/client5.svg';
+import arrow from '../Page/images/strelk.svg';
 
 const clients = [
-    { id: 1, client: client1 },
-    { id: 2, client: client2 },
-    { id: 3, client: client3 },
-    { id: 4, client: client4 },
-    { id: 5, client: client5 },
-    { id: 6, client: client1 },
-    { id: 7, client: client2 },
-    { id: 8, client: client3 },
+  { id: 1, client: client1 },
+  { id: 2, client: client2 },
+  { id: 3, client: client3 },
+  { id: 4, client: client4 },
+  { id: 5, client: client5 },
+  { id: 6, client: client1 },
+  { id: 7, client: client2 },
+  { id: 8, client: client3 },
 ];
 
 export default function Clients() {
+  const mapClientRef = useRef(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const scrollLeft = () => {
+    if (mapClientRef.current) {
+      mapClientRef.current.scrollLeft -= 300;
+      setScrollPosition(mapClientRef.current.scrollLeft);
+    }
+  };
+
+  const scrollRight = () => {
+    if (mapClientRef.current) {
+      mapClientRef.current.scrollLeft += 300;
+      setScrollPosition(mapClientRef.current.scrollLeft);
+    }
+  };
+
   return (
-    <div className='mapClient'>
-      {clients.map((el) => (
-        <div key={el.id}>
-          <img src={el.client} className='imgClient' />
-        </div>
-      ))}
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <div onClick={() => scrollLeft()}>
+        <img
+          src={arrow}
+          style={{ cursor: 'pointer', marginRight: '15px' }}
+          alt='left arrow'
+        />
+      </div>
+      <div className='mapClient' ref={mapClientRef}>
+        {clients.map((el) => (
+          <div key={el.id}>
+            <img
+              src={el.client}
+              className='imgClient'
+              alt={`Client ${el.id}`}
+            />
+          </div>
+        ))}
+      </div>
+      <div onClick={() => scrollRight()}>
+        <img
+          src={arrow}
+          style={{
+            transform: 'rotate(180deg)',
+            cursor: 'pointer',
+            marginLeft: '30px',
+          }}
+          alt='right arrow'
+        />
+      </div>
     </div>
   );
 }
--- client/src/Components/Reciews.jsx
+++ client/src/Components/Reciews.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import imgDefault from '../Page/images/imageDefault.png';
 import Rating from '@mui/material/Rating';
+import arrow from '../Page/images/strelk.svg';
 
 const reciews = [
   {
@@ -70,21 +71,69 @@ const reciews = [
 ];
 
 export default function Reciews() {
+  const mapReciewsRef = useRef(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const scrollLeft = () => {
+    if (mapReciewsRef.current) {
+      mapReciewsRef.current.scrollLeft -= 500;
+      setScrollPosition(mapReciewsRef.current.scrollLeft);
+    }
+  };
+
+  const scrollRight = () => {
+    if (mapReciewsRef.current) {
+      mapReciewsRef.current.scrollLeft += 500;
+      setScrollPosition(mapReciewsRef.current.scrollLeft);
+    }
+  };
+
   return (
-    <div className='mapReciews'>
-      {reciews.map((el) => (
-        <div key={el.id} className='oneRec'>
-          <Rating name='read-only' value={el.rating} readOnly style={{marginBottom:'20px'}}/>
-          <div style={{width:'300px',marginBottom:'20px'}}>{el.txt}</div>
-          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <img src={el.photo} className='defaultPhoto' />
-            <div>
-              <div style={{color:'#BE713C'}}>{el.name}</div>
-              <div>{el.job}</div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <div onClick={() => scrollLeft()}>
+        <img
+          src={arrow}
+          style={{ cursor: 'pointer', marginRight: '15px' }}
+          alt='left arrow'
+        />
+      </div>
+      <div className='mapReciews' ref={mapReciewsRef}>
+        {reciews.map((el) => (
+          <div key={el.id} className='oneRec'>
+            <Rating
+              name='read-only'
+              value={el.rating}
+              readOnly
+              style={{ marginBottom: '20px' }}
+            />
+            <div style={{ width: '300px', marginBottom: '20px' }}>{el.txt}</div>
+            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+              <img src={el.photo} className='defaultPhoto' />
+              <div>
+                <div style={{ color: '#BE713C' }}>{el.name}</div>
+                <div>{el.job}</div>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div onClick={() => scrollRight()}>
+        <img
+          src={arrow}
+          style={{
+            transform: 'rotate(180deg)',
+            cursor: 'pointer',
+            marginLeft: '30px',
+          }}
+          alt='right arrow'
+        />
+      </div>
     </div>
   );
 }
